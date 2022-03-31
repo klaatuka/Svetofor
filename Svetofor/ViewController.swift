02b46fbd31313686cca89d1ac164770142e1df60
klaatuka <klaatuka82@GMAIL.COM>
2022-03-31 16:19:02 +0300

    2 buttons

--- Svetofor/ViewController.swift
+++ Svetofor/ViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+enum CurrenLight {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet var redLight: UIView!
@@ -14,8 +18,17 @@ class ViewController: UIViewController {
     @IBOutlet var greenLight: UIView!
     
     @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrenLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+        
+        
         // Do any additional setup after loading the view.
     }
 

e33632bbe0393832ab4cdcee2d41034cc636e76f
klaatuka <klaatuka82@GMAIL.COM>
2022-03-31 16:01:49 +0300

    Svetofor 1 interface and button

--- Svetofor/ViewController.swift
+++ Svetofor/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
